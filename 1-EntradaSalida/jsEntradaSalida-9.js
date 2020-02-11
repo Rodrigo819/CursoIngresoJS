6d734eba8d89df96ce6df76a6a9a837ec81d7830
damian <damian1999ojeda@gmail.com>
2020-02-11 14:01:06 -0300

    10/2

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,20 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var sueldo ;
 
+	var porcentaje ; 
+
+
+	sueldo = document.getElementById("sueldo").value; 
+
+	sueldo = parseInt (sueldo);
+
+
+	porcentaje = sueldo * 0.10 ;
+
+	sueldo = sueldo + porcentaje ; 
+
+	document.getElementById("resultado").value= sueldo ;
 	
 }

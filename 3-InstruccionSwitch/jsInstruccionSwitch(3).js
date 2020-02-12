faee31887994d0a19cda704548863ab4a2e48d6b
alumno <alumno@utnfra.com.ar>
2020-02-11 22:03:08 -0300

    11/2/2

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -5,7 +5,23 @@ var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 	
-	
+	switch(mesDelAño){
+        case"Febrero":
+        alert("Este mes no tiene más de 29 días");
+        break
+        case"Enero":
+        case"Marzo":
+        case"Abril":
+        case"mayo":
+        case"Junio":
+        case"Julio":
+        case"Agosto":
+        case"Septiembre":
+        case"Octubre":
+        case"Noviembre":
+        case"Diciembre":
+        alert("Este mes tiene 30 o más días");
+    }
 
 
 }//FIN DE LA FUNCIÓN

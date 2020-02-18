eb27dc02a0f59fbd62accf72bb629b6028e2f74e
alumno <alumno@utnfra.com.ar>
2020-02-18 20:26:23 -0300

    18/2

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -3,12 +3,24 @@ function mostrar()
 
 	var contador=0;
 	// declarar variables
+	var maximo;
+	var minimo;
 	
 	var respuesta='si';
 
 	while(respuesta!='no')
 	{
-		
+	while(confirm("Quiere agregar algun numero")){
+	numero = prompt("Ingrese un numero");
+	numero = parseInt(numero);
+    while(isNaN(numero)){
+	numero = prompt("Ingrese un numero valido");
+	numero = parseInt(numero);
+
+}	
+contador++;
+acumulador += numero;
+}	
 	
 	}
 
